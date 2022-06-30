0fdd56c23fc54c97d822d01ef206ed127bf0c320
“carolemacharia” <carolemacharia@gmail.com>
2022-06-30 11:09:52 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-/**
- *
- *  *  main - Entry point
- *
- *   *  Description: prints "\"Programming is like building a multilingual puzzle"
- *
- *    *  Return: 0
- *
- *     */
+/*
+  main - Entry point  Description: prints "\"Programming is like building a multilingual puzzle"  Return: 0
+ */
 int main(void)
 
 {
