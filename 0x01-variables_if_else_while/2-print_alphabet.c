1089d96e8de1f70ea693ad0f806ad1cbda0d0a99
“carolemacharia” <carolemacharia@gmail.com>
2022-07-01 16:20:21 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
-	char ch;
-        printf("Alphabets from a - z are: \n");
-	for(ch='a'; ch<='z'; ch++)
-	{
-	printf("%c\n", ch);
-	}
-	return 0;
+ char ch;
+ printf("Alphabets from a - z are: \n");
+ for(ch='a'; ch<='z'; ch++)
+ {
+ printf("%c\n", ch);
+ }
+ return 0;
 }
