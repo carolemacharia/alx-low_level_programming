c4cfbed4055fc7884c9b2a15351cb21e057c6b68
“carolemacharia” <carolemacharia@gmail.com>
2022-07-01 16:40:30 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+	int ch;
+clrscr()
+printf(“Print all uppercase letters followed by all lowercase letters on the next line :\n”);
+/* print uppercase letters */
+for (ch= ‘A’; ch<= ‘Z’; ch++)
+putchar(ch);
+putchar (‘\n’ ) ;
+/* print lowercase letters */
+for (ch= ‘a’; ch<= ‘z’; ch++)
+putchar(ch);
+putchar (‘\n’) ;
+getch();
+}
