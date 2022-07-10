b2c428d4809893233d299d10010a5a5093d62fe5
“carolemacharia” <carolemacharia@gmail.com>
2022-07-10 13:40:01 -0700

    debugging task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+/**main tests functions that print if an integer is positive or negative
+*returns 0
+*/
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c)
+
 #endif /* MAIN_H */
