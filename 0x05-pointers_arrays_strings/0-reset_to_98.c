0d86d847d6330d73cd4cbb8c5537d9afe1828917
“carolemacharia” <carolemacharia@gmail.com>
2022-07-11 12:53:53 -0700

    reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
 * Description: reset_to_98() takes a pointer to an int as parameter and
 * updates the value it points to to 98
-* *n points to the memory address
+* @n: pointer to reset to 98
 */
 void reset_to_98(int *n)
 {
